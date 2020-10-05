5a42ea28dd6c92dc133f630c2f851dfdca8b6dda
M6code <benjamynfrank101@gmail.com>
2020-10-05 17:47:39 +0100

    07. Quiz: Practice Time

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,9 +1,32 @@
+import java.util.*
+//import kotlin.random.Random
+
 //fun main(args: Array<String>){
-//    println("Hello Kotlin")
+//    println("Hello ${args[0]}")
+// if (args[0].toInt() < 12) println("Good morning, Kotlin")
+// else println("Good night, Kotlin" )
 //}
 
 //fun main(){
 //    println("Hello Kotlin")
 //}
 
-fun main() = println("Hello Kotlin")
+//fun main() = println("Hello Kotlin")
+
+fun main() {
+//    var time = 23
+//    println("Good ${if (time < 12) "Morning" else "Evening"} Kotlin")
+    feedTheFish()
+}
+
+fun feedTheFish(){
+    val day = "Tuesday"
+    val food = "pellets"
+    println("Today is ${randomDay()} and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+//    return week[Random.nextInt(7)] // Random Kotlin way
+    return week[Random().nextInt(7)] // Random Java Way
+}
--- src/main/kotlin/PracticeFunction.kt
+++ src/main/kotlin/PracticeFunction.kt
@@ -1,7 +1,10 @@
 import java.util.*
 
-fun main(){
-    when(dayOfWeek()){
+//import kotlin.random.Random
+
+
+fun main() {
+    when (dayOfWeek()) {
         1 -> println("Sunday")
         2 -> println("Monday")
         3 -> println("Tuesday")
@@ -11,7 +14,37 @@ fun main(){
         7 -> println("Saturday")
         else -> println("Day Not Found")
     }
+
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortuneCookie()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
+
+    //println("Your fortune is: ${getFortuneCookie()}")
 }
+
+fun getFortuneCookie(): String {
+    val cookies = listOf("You will have a great day.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Take it easy and enjoy life",
+        "Life na jeje no rush am ",
+        "Use your head wella and everything go work out",
+        "Today is a good day for exercising restraint.")
+    print("Enter your birthday: ")
+
+    val birthday = readLine()!!.toIntOrNull() ?: 1 // Receives value from console Check if birthday input is not Integer then assign 1 to birthday
+    // Same as above
+    //val birthday = readLine()?.toIntOrNull() ?: 1 // Receive value, check if it is null and set default to 1
+    val urCookiePos: Int = birthday % cookies.size //Divide the birthday by the number of fortunes, and use the remainder as the index for the fortune to return.
+    return cookies[urCookiePos] // Return the fortune
+//    or  return cookies[birthday.rem(cookies.size)]
+//    return cookies[Random().nextInt(6)]
+
+}
+
 
 //fun dayOfWeek() = println("What day is today?")
 
