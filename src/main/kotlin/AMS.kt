8f3866c1ae738828a2ff992056e3c0883b63a616
M6code <benjamynfrank101@gmail.com>
2020-10-09 18:05:27 +0100

    15. Quiz: Practice Time

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,4 +1,6 @@
 import java.util.*
+import kotlin.math.tan
+
 //import kotlin.random.Random
 
 //fun main(args: Array<String>){
@@ -17,9 +19,13 @@ fun main() {
 //    var time = 23
 //    println("Good ${if (time < 12) "Morning" else "Evening"} Kotlin")
     feedTheFish()
+    println(canAddFish(10.0, listOf(3, 3, 3))) // false
+    println(canAddFish(8.0, listOf(2, 2, 2), hasDecorations = false)) // true
+    println(canAddFish(9.0, listOf(1, 1, 3), 3)) // false
+    println(canAddFish(10.0, listOf(), 7, true)) // true
 }
 
-fun feedTheFish(){
+fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
@@ -27,7 +33,7 @@ fun feedTheFish(){
 
 fun fishFood(day: String): String {
 
-    return when(day){
+    return when (day) {
         "Monday" -> "flakes"
         "Tuesday" -> "pellets"
         "Wednesday" -> "redworms"
@@ -43,4 +49,12 @@ fun randomDay(): String {
     val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 //    return week[Random.nextInt(7)] // Random Kotlin way
     return week[Random().nextInt(7)] // Random Java Way
+}
+
+fun shouldChangeWater(day: String, temp: Int = 22, dirty: Int = 20): Boolean {
+    return true
+}
+
+fun canAddFish(tanksize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
+    return (tanksize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
--- src/main/kotlin/PracticeFunction.kt
+++ src/main/kotlin/PracticeFunction.kt
@@ -15,19 +15,24 @@ fun main() {
         else -> println("Day Not Found")
     }
 
-    var fortune: String
-    for (i in 1..10) {
-        var birthday = getBirthday();
-        fortune = when (birthday) {
-            28, 31 -> "28, 31 People Na wah ooo"
-            in 1..7 -> "First week people una to early ooo"
-            else -> getFortuneCookie(birthday)
-        }
-        println("\nYour fortune is: $fortune")
-        if (fortune.contains("Take it easy")) break
-    }
+//    var fortune: String
+//    for (i in 1..10) {
+//        var birthday = getBirthday();
+//        fortune = when (birthday) {
+//            28, 31 -> "28, 31 People Na wah ooo"
+//            in 1..7 -> "First week people una to early ooo"
+//            else -> getFortuneCookie(birthday)
+//        }
+//        println("\nYour fortune is: $fortune")
+//        if (fortune.contains("Take it easy")) break
+//    }
 
     //println("Your fortune is: ${getFortuneCookie()}")
+
+    println(whatShouldIDoToday("happy", "sunny"))
+    println(whatShouldIDoToday("sad"))
+    print("How do you feel?")
+    println(whatShouldIDoToday(readLine()!!))
 }
 
 fun getFortuneCookie(birthday: Int): String {
@@ -38,13 +43,14 @@ fun getFortuneCookie(birthday: Int): String {
         "Life na jeje no rush am ",
         "Use your head wella and everything go work out",
         "Today is a good day for exercising restraint.")
-    val position: Int = birthday % cookies.size //Divide the birthday by the number of fortunes, and use the remainder as the index for the fortune to return.
+    val position: Int =
+        birthday % cookies.size //Divide the birthday by the number of fortunes, and use the remainder as the index for the fortune to return.
     return cookies[position] // Return the fortune
 }
 
 fun getBirthday(): Int {
     print("Enter your birthday: ")
-    return readLine()!!.toIntOrNull() ?: 1;
+    return readLine()!!.toIntOrNull() ?: 1
 
     // Same as above
     // return readLine()?.toIntOrNull() ?: 1 // Receive value, check if it is null and set default to 1
@@ -56,3 +62,18 @@ fun getBirthday(): Int {
 //   return Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
 //}
 fun dayOfWeek(): Int = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+
+fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
+    return when {
+        isVeryHot(temperature) -> "go swimming"
+        isSadRainyCold(mood, weather, temperature) -> "stay in bed"
+        isHappySunny(mood, weather) -> "go for a walk"
+        else -> "Stay home and read."
+    }
+}
+
+
+fun isVeryHot(temperature: Int) = temperature > 35
+fun isSadRainyCold(mood: String, weather: String, temperature: Int) = mood == "sad" && weather == "rainy" && temperature == 0
+fun isHappySunny(mood: String, weather: String) = mood == "happy" && weather == "sunny"
+
